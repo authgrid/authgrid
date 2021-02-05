38d048efa54c5786bae8814977cdf9ea74994208
Nir Berko <nirberko@gmail.com>
2021-02-05 19:48:21 +0200

    mailer interface

--- packages/common/interfaces/authGrid.interfaces.ts
+++ packages/common/interfaces/authGrid.interfaces.ts
@@ -1,10 +1,11 @@
 import { IDriver } from './driver.interfaces';
+import { IMailer } from './mailer.interfaces';
 
 export interface IOptions {
   driver: IDriver;
   tokenSecret: string;
   refreshTokenSecret: string;
+  mailer: (params: IMailer) => Promise<any>;
   tokenExpires?: number;
   refreshTokenExpires?: number;
-  sendGridApiKey: string;
 }
---
+++ packages/common/interfaces/mailer.interfaces.ts
@@ -0,0 +1,5 @@
+export interface IMailer {
+  to: string;
+  subject: string;
+  html: string;
+}
